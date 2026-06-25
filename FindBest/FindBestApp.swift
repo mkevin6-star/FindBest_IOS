e5f6ca39bc04a2ff6a2e2964e45e373bb6181e55
Kevin Mathews <mkevin6@vt.edu>
2026-06-25 18:10:32 -0400

    Initial Commit

---
+++ FindBest/FindBestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindBestApp.swift
+//  FindBest
+//
+//  Created by Kevin Mathews on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct FindBestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
